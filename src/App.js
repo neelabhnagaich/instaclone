74af044ef3344f38c152c5e9e1d36e0b807172ac
neelabhnagaich <neelabhnagaich@gmail.com>
2021-07-17 10:48:04 +0530

    store created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+    return (
+        <View>
+            <Text>Hello from App</Text>
+        </View>
+    )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ src/RootApp.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import App from './App';
+
+
+const RootApp = () => {
+    return (
+        <View>
+            <App />
+        </View>
+    )
+}
+
+export default RootApp
+
+const styles = StyleSheet.create({})
