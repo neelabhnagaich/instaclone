b973d2251e15a658ce192de01f9928ab981e2f86
neelabhnagaich <neelabhnagaich@gmail.com>
2021-07-17 10:12:45 +0530

    Store created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Text,
+} from 'react-native';
+
+
+const App =() => {
+  return (
+    <>
+     <Text>Hello from App</Text>
+    </>
+  )
+};
+
+
+
+export default App;
