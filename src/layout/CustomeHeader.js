7248735b5658ce1a1c1101ff70ef8455fc2e2fa0
neelabhnagaich <neelabhnagaich@gmail.com>
2021-07-20 09:13:25 +0530

    custom header done

--- src/layout/CustomeHeader.js
+++ src/layout/CustomeHeader.js
@@ -1,14 +1,66 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import {
+    Body,
+    Right,
+    Button,
+    Icon,
+    Title,
+    Text,
+    Header
+} from 'native-base'
 
-const CustomeHeader = () => {
+import {connect} from 'react-redux'
+import propTypes from 'prop-types'
+import {signout} from '../action/auth'
+
+const CustomeHeader = ({authState,signout,navigation}) => {
     return (
-        <>
-            <Text>Hello from custome header</Text>
-        </>
+        <Header
+         androidStatusBarColor="#0f4c75"
+         style={{
+             backgroundColor:"#0f4c75"
+         }}
+        >
+            <Body>Social App</Body>
+            <Right>
+                {authState.isAuthenticated && (
+                    <>
+                    <Button
+                        transparent
+                        iconLeft
+                        onPress={()=>{navigation.navigate('AddPost')}}
+                    >
+                        <Text style={{color:"#fdcb9e"}}>Add Post</Text>
+                    </Button>
+                    <Button
+                        transparent
+                        iconLeft
+                        onPress={()=>{signout()}}
+                    >
+                        <Icon name="log-out-outline" style={{color:"red"}}></Icon>
+                    </Button>
+                    </>
+                )}
+            </Right>
+        </Header>
     )
 }
 
-export default CustomeHeader
+const mapStateToProps = (state)=>{
+    authstate:state.auth
+}
+
+const mapDispathToProps = {
+    signout
+}
+
+CustomeHeader.prototypes={
+    signout: propTypes.func.isRequired,
+    authState:propTypes.object.isRequired
+}
+
+
+export default connect(mapStateToProps,mapDispathToProps)(CustomeHeader)
 
 const styles = StyleSheet.create({})
